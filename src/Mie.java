336732ddcb6e719f51c4c05a369cff7f32a6bf95
tiaagustiani <tia.agustiani@unida.ac.id>
2021-12-18 18:03:20 +0700

    Forum Pertemuan 10

--- src/Mie.java
+++ src/Mie.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
 public class Mie {
 	public static void main (String[] args) {
-		int waktu=3;
 		boolean mie=true;
+		int waktu=14;
 
 		if (mie==true) {
 			if(waktu==3) {
 				System.out.println("Mie Matang Sempurna");
-			}if (mie==true) {
-				if (waktu<=2) {
-					System.out.println("Mie Belum Matang");
-				}if (mie==true) {
-					if (waktu>=4) {
-						System.out.println("Mie Terlalu Lembek");
-				}if (mie==true) 
-					if (waktu>=10) {
-						System.out.println("Mie Sudah Menjadi Bubur");
-					}
-				} else {
-					System.out.println("Tidak ada kondisi ");
-				}
+			}if (waktu<=2) {
+				System.out.println("Mie Belum Matang");
+			}if (waktu>=4) {
+				System.out.println("Mie Terlalu Lembek");
+			}if (waktu>=10) {
+				System.out.println("Mie Sudah Menjadi Bubur");
 			}
-
+		} else {
+			System.out.println("Tidak ada kondisi ");
 		}
 	}
+
 }
